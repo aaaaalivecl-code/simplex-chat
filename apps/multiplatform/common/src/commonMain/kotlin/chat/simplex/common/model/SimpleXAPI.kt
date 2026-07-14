0cf9f84790e7681e308d7b8b52027f5cd183cef1
shum <github.shum@liber.li>
2026-07-14 14:30:16 +0000

    multiplatform: per-server role toggles for self-hosted servers

--- apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/model/SimpleXAPI.kt
+++ apps/multiplatform/common/src/commonMain/kotlin/chat/simplex/common/model/SimpleXAPI.kt
@@ -4791,7 +4791,8 @@ data class UserServer(
   val preset: Boolean,
   val tested: Boolean? = null,
   val enabled: Boolean,
-  val deleted: Boolean
+  val deleted: Boolean,
+  val roles: ServerRoles? = null
 ) {
   @Transient
   private val createdAt: Date = Date()
